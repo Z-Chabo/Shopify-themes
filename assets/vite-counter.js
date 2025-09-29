55bd93f32766a16adf1f697186e63dc14f45fd04
Zeidan chabo <zeidanchabo@gmail.com>
2025-09-28 21:34:42 -0400

    dsadsad

--- assets/vite-counter.js
+++ assets/vite-counter.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import{r as n,j as t,c as o}from"./vite-client.js";function s(){const[e,r]=n.useState(0);return t.jsxs("div",{children:[t.jsx("button",{onClick:()=>r(e+1),children:"Increment"}),t.jsxs("p",{children:["Count: ",e]})]})}o.createRoot(document.getElementById("counter")).render(t.jsx(n.StrictMode,{children:t.jsx(s,{})}));
-=======
 import{r as n,j as t,c as o}from"./vite-client.js";function s(){const[e,r]=n.useState(0);return t.jsxs("div",{children:[t.jsx("button",{onClick:()=>r(e+1),children:"Increment"}),t.jsxs("p",{children:["Count: ",e]})]})}o.createRoot(document.getElementById("Counter")).render(t.jsx(n.StrictMode,{children:t.jsx(s,{})}));
->>>>>>> 66c0f64c047d6051bc84771f194d58b66a03352d
+
+import{r as n,j as t,c as o}from"./vite-client.js";function s(){const[e,r]=n.useState(0);return t.jsxs("div",{children:[t.jsx("button",{onClick:()=>r(e+1),children:"Increment"}),t.jsxs("p",{children:["Count: ",e]})]})}o.createRoot(document.getElementById("counter")).render(t.jsx(n.StrictMode,{children:t.jsx(s,{})}));
